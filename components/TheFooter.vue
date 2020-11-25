d41671dcb0d0b63711bca781781309667bc42f26
Guillermo Croppi <guillermocroppi@gmail.com>
2020-11-25 15:12:22 -0300

    Updated footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,9 +4,7 @@
       <a href="https://metropolitana.org.ar/" target="_blank"><img src="~/assets/fm_logo.png" class="image is-centered mb-3" alt="Fundación Metropolitana"></a></div>
       <div class="content has-text-centered">
         <b>Fundación Metropolitana</b>
-        <p>54-11-6091 – 7550 / 7551</p>
         <p><a href="mailto:info@metropoitano.org.ar" class="has-text-dark">info@metropoitano.org.ar</a></p>
-        <p>(C1036AAO) Ciudad de Buenos Aires</p>
       </div>
     </div>
   </footer>
